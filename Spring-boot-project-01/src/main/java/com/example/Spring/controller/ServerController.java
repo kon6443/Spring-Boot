5ecc280c46d48f5ba1c3f48ca8e0109953b39105
Onam Kwon <kon6443@gmail.com>
2022-10-05 18:23:37 +0900

    Java spring boot server first commit.

---
+++ Spring-boot-project-01/src/main/java/com/example/Spring/controller/ServerController.java
@@ -0,0 +1,13 @@
+package com.example.Spring.boot.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject01Application.class, args);
+	}
+
+}

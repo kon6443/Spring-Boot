52870a48f4ed6ddf9e9361e90debc0e6805ce88b
Onam Kwon <kon6443@gmail.com>
2022-11-03 22:15:43 +0900

    Keep working on jwt

--- Spring-boot-project-01/src/main/java/com/example/Spring/boot/project1/config/SecurityConfig.java
+++ Spring-boot-project-01/src/main/java/com/example/Spring/boot/project1/config/SecurityConfig.java
@@ -1,23 +1,54 @@
 package com.example.Spring.boot.project1.config;
 
+import com.example.Spring.boot.project1.jwt.JwtAuthenticationFilter;
+import com.example.Spring.boot.project1.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private JwtTokenProvider jwtTokenProvider;
+    // authenticationManager를 Bean 등록합니다.
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                .antMatchers("/").permitAll()
-//                .antMatchers("/board").permitAll()
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        // 일반적인 루트가 아닌 다른 방식으로 요청시 거절, header에 id, pw가 아닌 token(jwt)을 달고 간다. 그래서 basic이 아닌 bearer를 사용한다.
+        /**
+         * antMatchers() : 해당 URL로 요청 시 설정을 해준다.
+         * authenticated() : andMatchers에 속해있는 URL로 요청이 오면 인증이 필요하다고 설정한다.
+         * hasRole() : andMatchers에 속해있는 URL로 요청이 들어오면 권한을 확인한다.
+         * addFilterBefore() : 필터를 등록한다. 스프링 시큐리티 필터링에 등록해주어야 하기 때문에, 여기에 등록해주어야 한다.
+         *                     파라미터는 2가지가 들어간다. 왼쪽은 커스텀한 필터링이 들어간다. 오른쪽에 등록한 필터전에 커스텀필터링이 수행된다.
+         * http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) : 세션을 사용하지 않는다고 설정한다.
+         */
+        http.httpBasic().disable()
+                .authorizeRequests()// 요청에 대한 사용권한 체크
+                .antMatchers("/test").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user").permitAll()
-//                .antMatchers("/user/signin").permitAll()
-        ;
-        return http.build();
+//                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class); // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
+        // + 토큰에 저장된 유저정보를 활용하여야 하기 때문에 CustomUserDetailService 클래스를 생성합니다.
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
     }
 }
